655a5b3aaa12adb45055db36395f3646c72493ce
LongTran2003 <quylongtranngoc07@gmail.com>
2025-11-26 18:06:10 +0700

    ExamFormat

---
+++ AttaEduSystem_BE/AttaEduSystem.Models/DTOs/ExamFormat/ExamFormatSchema.cs
@@ -0,0 +1,11 @@
+﻿namespace AttaEduSystem.Models.DTOs.ExamFormat
+{
+    public class ExamFormatSchema
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string? TimeLimit { get; set; }
+        public string? Instructions { get; set; }
+        public List<ExamSection> Sections { get; set; } = new();
+    }
+}
