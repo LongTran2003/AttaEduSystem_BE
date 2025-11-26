b8bbc1897c60b11b1eb06077c77422f439c4151c
LongTran2003 <quylongtranngoc07@gmail.com>
2025-11-26 16:43:42 +0700

    Update Admin

---
+++ AttaEduSystem_BE/AttaEduSystem.DataAccess/Migrations/20251126094225_ChangeAdminName.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttaEduSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAdminName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "8fa7c7bb-daa5-a660-bf02-82301a5eb32a", "Movok-Admin" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "Movok-Admin");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Address", "BirthDate", "ConcurrencyStamp", "Email", "EmailConfirmed", "FullName", "Gender", "ImageUrl", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "OtpCode", "OtpExpiry", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "Status", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "AttaEdu-Admin", 0, "123 Admin St", new DateTime(2001, 6, 5, 0, 0, 0, 0, DateTimeKind.Utc), "fd17d973-5574-4d65-8052-cca23d5a5e13", "admin@gmail.com", true, "Admin", null, "https://example.com/avatar.png", true, null, "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", null, null, "AQAAAAIAAYagAAAAED+UUyl8k1ueJHG8aE1D2tbAc0hDJzGFp/GG750NHRc6w1eNEmPnNkcnq2iEc6kBuQ==", "1234567890", true, "0f80909d-5696-4530-ae7f-a3061887ba5b", "Active", false, "admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "8fa7c7bb-daa5-a660-bf02-82301a5eb32a", "AttaEdu-Admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "8fa7c7bb-daa5-a660-bf02-82301a5eb32a", "AttaEdu-Admin" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "AttaEdu-Admin");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Address", "BirthDate", "ConcurrencyStamp", "Email", "EmailConfirmed", "FullName", "Gender", "ImageUrl", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "OtpCode", "OtpExpiry", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "Status", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "Movok-Admin", 0, "123 Admin St", new DateTime(2001, 6, 5, 0, 0, 0, 0, DateTimeKind.Utc), "c0582e48-dcd9-4cc0-a7de-dc34409d0c68", "admin@gmail.com", true, "Admin", null, "https://example.com/avatar.png", true, null, "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", null, null, "AQAAAAIAAYagAAAAEICDDTnIXSjMLtsZptDFSUAn5XWQlIbBMv/dYC7FgXE6kuHNOxD5Sjd4G+up0gdLoA==", "1234567890", true, "ebb0f572-3d74-4475-aea4-21b3cf75a67e", "Active", false, "admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "8fa7c7bb-daa5-a660-bf02-82301a5eb32a", "Movok-Admin" });
+        }
+    }
+}
