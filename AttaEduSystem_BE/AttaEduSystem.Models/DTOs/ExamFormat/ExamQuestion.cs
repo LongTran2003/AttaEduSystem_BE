b1c0eb540b25c193e377102068b87a3bd361819b
LongTran2003 <quylongtranngoc07@gmail.com>
2025-11-26 18:06:10 +0700

    ExamFormat

---
+++ AttaEduSystem_BE/AttaEduSystem.Models/DTOs/ExamFormat/ExamQuestion.cs
@@ -0,0 +1,10 @@
+﻿namespace AttaEduSystem.Models.DTOs.ExamFormat
+{
+    public class ExamQuestion
+    {
+        public string Code { get; set; } = string.Empty; // Câu 1, Câu 2...
+        public string Requirement { get; set; } = string.Empty;
+        public decimal? Points { get; set; }
+        public string? SubRequirement { get; set; } // ví dụ “Câu hỏi nhỏ” nếu có
+    }
+}
---
+++ AttaEduSystem_BE/AttaEduSystem.Models/DTOs/ExamFormat/ExamSection.cs
@@ -0,0 +1,9 @@
+﻿namespace AttaEduSystem.Models.DTOs.ExamFormat
+{
+    public class ExamSection
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? TotalPoints { get; set; }
+        public List<ExamQuestion> Questions { get; set; } = new();
+    }
+}
