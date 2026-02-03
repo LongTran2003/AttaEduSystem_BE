ad2c2a351ef5779ca868627bd4a1a48d688fd9e7
LongTran2003 <quylongtranngoc07@gmail.com>
2026-02-03 21:39:41 +0700

    Add Upload user avatar API

--- AttaEduSystem_BE/AttaEduSystem.Services/IServices/IProfileService.cs
+++ AttaEduSystem_BE/AttaEduSystem.Services/IServices/IProfileService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using AttaEduSystem.Models.DTOs;
 using AttaEduSystem.Models.DTOs.Profile;
+using Microsoft.AspNetCore.Http;
 
 namespace AttaEduSystem.Services.IServices;
 
@@ -9,4 +10,5 @@ public interface IProfileService
     Task<ResponseDto> GetUserProfile(ClaimsPrincipal userPrincipal);
     Task<ResponseDto> UpdateUserProfile(ClaimsPrincipal userPrincipal, UpdateUserProfileDto updateUserProfileDto);
     Task<ResponseDto> RefreshAccessToken(RefreshTokenDto refreshTokenDto);
+    Task<ResponseDto> UploadAvatar(ClaimsPrincipal userPrincipal, IFormFile avatarFile);
 }
