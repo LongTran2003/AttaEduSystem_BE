066daf5c4331f2a99a86977f09f56e9e8309d686
LongTran2003 <quylongtranngoc07@gmail.com>
2026-02-11 19:23:06 +0700

    Add Dashboard for Admin

---
+++ AttaEduSystem_BE/AttaEduSystem.Models/DTOs/Admin/Dashboards/ExamCreationChartDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AttaEduSystem.Models.DTOs.Admin.Dashboards
+{
+    public class ExamCreationChartDto
+    {
+        public string Month { get; set; } = null!;
+        public string MonthLabel { get; set; } = null!;
+        public int ScannedExams { get; set; } // ExamPaper
+        public int AiGeneratedExams { get; set; } // GeneratedExamPaper
+        public int Total { get; set; }
+    }
+}
