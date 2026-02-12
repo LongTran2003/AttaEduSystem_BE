3399ab160642c051ba2a8d381e05300c62c0b5fe
LongTran2003 <quylongtranngoc07@gmail.com>
2026-02-12 19:49:03 +0700

    Add ChatBox AI

---
+++ AttaEduSystem_BE/AttaEduSystem.Models/DTOs/ChatBox/ConversationDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AttaEduSystem.Models.DTOs.ChatBox
+{
+    public class ConversationDetailDto
+    {
+        public Guid ChatConversationId { get; set; }
+        public string? Title { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public List<ChatMessageDto> Messages { get; set; } = new();
+    }
+}
---
+++ AttaEduSystem_BE/AttaEduSystem.Models/Enums/MessageRole.cs
@@ -0,0 +1,9 @@
+﻿namespace AttaEduSystem.Models.Enums
+{
+    public enum MessageRole
+    {
+        User = 0,
+        Assistant = 1,
+        System = 2
+    }
+}
